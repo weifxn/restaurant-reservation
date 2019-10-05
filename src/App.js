896722122c826583f86a944774f3aa79afa0dfe8
wf <weifan2647@gmail.com>
2019-10-05 19:42:10 +0800

    init firebase, router and env files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './containers/Home'
+
+import { Helmet } from 'react-helmet'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <title>Online Reservation</title>
+        <meta name="description" content="This is a online resstaurant reservation website built by wf with ReactJS & Firebase" />
+      </Helmet>
+      <Router >
+        <Route path="/" exact component={Home} />
+      </Router>
     </div>
   );
 }
---
+++ src/containers/Home.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import Firebase from '../firebase'
+
+export default () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
